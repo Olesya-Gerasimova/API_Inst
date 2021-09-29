03832d0bcca0ef2ce52cac97eba31e5192109d5e
Olesya-Gerasimova <ogerasimova966@gmail.com>
2021-09-29 16:21:54 +0300

    added API

---
+++ API_Inst/Backend/Service.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+
+namespace API_Inst
+{
+    public class Service
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Сообщение не дошло", "ogerasimova966@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 25, false);
+                await client.AuthenticateAsync("ogerasimova966@gmail.com", "password");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
